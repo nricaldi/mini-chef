b0af2bf4e0be76e02ce0d08b390066fb583de6b3
Nico Ricaldi <nicoricaldi.dev@gmail.com>
2026-02-24 00:10:54 -0500

    Add random sleep function. Add the click to download video.

--- main.py
+++ main.py
@@ -5,6 +5,9 @@ from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.common.action_chains import ActionChains
 
+def random_sleep(min=0.0, max=5.0):
+    random_num = random.uniform(min, max)
+    time.sleep(random_num)
 
 def main():
     print('Hello from mini-chef!')
@@ -13,23 +16,32 @@ def main():
     actions = ActionChains(driver)
 
     snapinsta_url = 'https://snapinsta.to/en/instagram-reels-downloader'
+    snapinsta_url = 'https://www.instagram.com/reel/DTwaCXHESPd/?igsh=eXVjaTZ3azJwMGd1'
     driver.get(snapinsta_url)
 
     input_element = driver.find_element(By.ID, 's_input')
+    random_sleep()
 
-    time.sleep(random.uniform(1.0, 3.0))
+    actions.move_to_element(input_element).perform()
+    random_sleep()
 
     actions.click(input_element).perform()
-
-    time.sleep(random.uniform(1.0, 2.0))
+    random_sleep()
 
     url_to_download = 'https://www.instagram.com/reel/DTwaCXHESPd/?igsh=eXVjaTZ3azJwMGd1'
     actions.send_keys(url_to_download).perform()
+    random_sleep()
 
-    time.sleep(random.uniform(1.0, 4))
+    btn_element = driver.find_element(By.CLASS_NAME, 'btn-default')
+    random_sleep(0.0, 1)
+
+    actions.move_to_element(btn_element).perform()
+    random_sleep()
+
+    actions.click(btn_element).perform()
+    random_sleep()
 
     driver.quit()
-
 
 if __name__ == '__main__':
     main()
