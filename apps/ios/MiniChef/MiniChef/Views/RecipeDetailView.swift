07f8fa51a211eae1f16f0469081da3848bfbd3f7
Nico Ricaldi <nicoricaldi.dev@gmail.com>
2026-04-13 23:34:07 -0400

    feat: fetch recipe from SwiftData by UUID

--- apps/ios/MiniChef/MiniChef/Views/RecipeDetailView.swift
+++ apps/ios/MiniChef/MiniChef/Views/RecipeDetailView.swift
@@ -6,14 +6,33 @@
 //
 
 import SwiftUI
+import SwiftData
 
 struct RecipeDetailView: View {
-    var recipeID: UUID
+    let recipeID: UUID
+    @Query private var  recipes: [Recipe]
+
+    init(recipeID: UUID) {
+        self.recipeID = recipeID
+
+        _recipes = Query(filter: #Predicate<Recipe> { recipe in
+            recipe.id == recipeID
+        })
+    }
 
     var body: some View {
         VStack(spacing: 12) {
             Text("Hello from RecipeDetailView mate")
-            Text("Currently viewing \(recipeID)")
+
+            if let recipe = recipes.first {
+                Text(recipe.title)
+                    .font(.largeTitle)
+                    .bold()
+                Text(recipe.desc)
+                    .foregroundStyle(.secondary)
+            } else {
+                ContentUnavailableView("Not Found", systemImage: "questionmark")
+            }
         }
     }
 }
